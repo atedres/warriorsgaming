15b4e5ad502f317433468a6121d4193f32a35027
ayman IT <aymanradi93@gmail.com>
2025-12-20 16:20:42 +0000

    dans le profil client le tesxte mon profil n'est popas bien resizer

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -11,7 +11,7 @@ export function PageHeader({ title, description, children, className }: PageHead
   return (
     <div className={cn("flex flex-col md:flex-row items-start md:items-center justify-between gap-4 py-8", className)}>
       <div className="grid gap-1">
-        <h1 className="font-headline text-2xl md:text-3xl font-bold tracking-tight">{title}</h1>
+        <h1 className="font-headline text-xl md:text-3xl font-bold tracking-tight">{title}</h1>
         {description && <p className="text-muted-foreground">{description}</p>}
       </div>
       {children && <div className="flex items-center gap-2">{children}</div>}
